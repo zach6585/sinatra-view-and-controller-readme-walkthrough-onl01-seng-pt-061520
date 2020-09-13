b7672fd9ea58b9cd89d78bdcacbaaa7ebfbc6627
zachary mines <zmines@wesleyan.edu>
2020-09-13 16:39:12 +0000

    Automatically backed up by Learn

--- app.rb
+++ app.rb
@@ -12,6 +12,7 @@ class App < Sinatra::Base
 
   get '/friends' do
     @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth']
+    erb :friends
 
   end
 end
